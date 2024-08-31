a52ab9f509d17929f139a297d36802297616cfb2
kellysalas <ksalas039@gmail.com>
2024-08-31 10:41:03 -0300

    Solucion del problema

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 const div = document.querySelector("div");
+const button = document.querySelector("button");
 
-div.addEventListener("click", function() {
+div.addEventListener("click", function(event) {
     alert('¡Hola! Soy el div');
+});
+
+button.addEventListener("click", function(event) {
+    event.stopPropagation();
+    alert("¡Hola!");
 });
